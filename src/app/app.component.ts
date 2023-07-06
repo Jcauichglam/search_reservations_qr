79e3999c2c9aa3a3e09cb75e67aa437b06c33829
Jcauichglam <jimmi_pumas@hotmail.com>
2023-07-06 12:12:16 -0500

    base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { environment } from 'src/environments/environment';
+import {
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token,
+} from '@capacitor/push-notifications';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +15,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'search_reservations_qr';
+  message: any = null;
+  constructor() { }
+  ngOnInit(): void {
+    this.requestPermission();
+    this.listen();
+  }
+  requestPermission() {
+    const messaging = getMessaging();
+    getToken(messaging,
+      { vapidKey: environment.firebase.vapidKey }).then((currentToken) => {
+          if (currentToken) {
+            console.log("Hurraaa!!! we got the token.....");
+            console.log(currentToken);
+            this.subscribeTokenToTopic(currentToken, "aerotek");
+          } else {
+            console.log('No registration token available. Request permission to generate one.');
+          }
+        }).catch((err) => {
+          console.log('An error occurred while retrieving token. ', err);
+        });
+  }
+
+  subscribeTokenToTopic(token, topic) {
+    fetch('https://iid.googleapis.com/iid/v1/'+token+'/rel/topics/'+topic, {
+      method: 'POST',
+      headers: new Headers({
+        'Authorization': 'key=AAAA5GyNeHU:APA91bFixIJ81__9OGoEffXyuEHPdEcGhb2lOfocMswAoGmZIxJKh92fWS5dYjl-6DfWSNiDSpgN98PDghbRzLZRq45kbOE3Bes6L0zMjViWWEvXr1SLRIZnmxrnnbGMlUvzEu1Bfo1k'
+      })
+    }).then(response => {
+      if (response.status < 200 || response.status >= 400) {
+        throw 'Error subscribing to topic: '+response.status + ' - ' + response.text();
+      }
+      console.log('Subscribed to "'+topic+'"');
+    }).catch(error => {
+      console.error(error);
+    })
+  }
+
+  listen() {
+    const messaging = getMessaging();
+    onMessage(messaging, (payload) => {
+      console.log('Message received. ', payload);
+      this.message = payload;
+    });
+  }
+
 }
